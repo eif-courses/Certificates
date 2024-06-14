62a5ba6801e64fd12746c3bd5e286817d7611e55
Marius <m.gzegozevskis@gmail.com>
2024-06-14 15:53:46 +0300

    dt

--- Certificates/Program.cs
+++ Certificates/Program.cs
@@ -9,7 +9,8 @@ QuestPDF.Settings.License = LicenseType.Community;
 
 var imageData =
     File.ReadAllBytes("C:\\Users\\Marius\\RiderProjects\\Certificates\\Certificates\\resources\\5718079.jpg");
-
+// REMU PAIESKA
+// https://pngtree.com/freepng/gold-line-blue-background-certificate-border_6491225.html 
 Console.WriteLine(imageData.Length);
 
 Document.Create(document =>
@@ -22,7 +23,7 @@ Document.Create(document =>
             layers
                 .Layer()
                 .Image(Image.FromFile(
-                    "C:\\Users\\Marius\\RiderProjects\\Certificates\\Certificates\\resources\\frame.webp"));
+                    "C:\\Users\\Marius\\RiderProjects\\Certificates\\Certificates\\resources\\naujas.png")).FitUnproportionally();
 
 
             layers
@@ -32,38 +33,136 @@ Document.Create(document =>
                     col.Item()
                         .AlignCenter()
                         .Width(70, Unit.Millimetre)
-                        .PaddingTop(20, Unit.Millimetre)
+                        .PaddingTop(25, Unit.Millimetre)
                         .Image(Image.FromFile(
                             "C:\\Users\\Marius\\RiderProjects\\Certificates\\Certificates\\resources\\logo.jpg"));
                     col.Item()
                         .AlignCenter()
                         .PaddingTop(30)
-                        .Text("Certificate of Completion")
-                        .FontSize(36)
-                        .Bold()
-                        .FontColor(Colors.Black);
+                        .Text("QUALITY CERTIFICATE")
+                        .Bold().FontSize(30).FontColor(Colors.Blue.Darken4);
 
                     col.Item()
                         .AlignCenter()
                         .PaddingTop(10)
-                        .Text("This is to certify that:")
-                        .FontSize(24)
+                        .Text("This is hereby to certify that")
+                        .FontSize(14)
                         .FontColor(Colors.Black);
 
                     col.Item()
                         .AlignCenter()
                         .PaddingTop(5)
-                        .Text("John Doe")
-                        .FontSize(30)
+                        .Text("Practice Enterprise: Viko Bankas")
+                        .FontSize(16)
                         .Bold()
                         .FontColor(Colors.Black);
 
                     col.Item()
                         .AlignCenter()
                         .PaddingTop(5)
-                        .Text("has successfully completed the course.")
-                        .FontSize(24)
+                        .Text("PE Trainer: Marius Ignatonis")
+                        .FontSize(14)
                         .FontColor(Colors.Black);
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("PE Students: Petras Petriukas, Ona Onaitienė, Jonas Jonaitis")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("Institution: Viko")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    
+                    
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("Country: Lietuva")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("has participated in external quality management auditing procedures")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("in the field SALES MANAGEMENT")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                  
+                    
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("according to PEN-QM quality system www.pen......")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("and achieved BRONZE level gathering XX points from max 50")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(20)
+                        .Text("achievements are described in the other side of this certificate")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .MaxWidth(170, Unit.Millimetre)
+                        .Text("Auditors/auditor: Bronius Bronislavas, Viko Elektronikos ir informatikos fakultetas, Lietuva")
+                        .FontSize(14)
+                        .FontColor(Colors.Black);
+                    
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("Certificate No. FE98521325")
+                        .FontSize(12)
+                        .FontColor(Colors.Black);
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("Date: 06/10/2024")
+                        .FontSize(12)
+                        .FontColor(Colors.Black);
+                    
+                    col.Item()
+                        .AlignCenter()
+                        .PaddingTop(5)
+                        .Text("Validation period: 06/10/2026")
+                        .FontSize(12)
+                        .FontColor(Colors.Black);
+                    
+                    
+                    
+                    
+                    
                 });
 
 
@@ -79,12 +178,12 @@ Document.Create(document =>
             //     });
 
             // layer above the main content    
-            layers
-                .Layer()
-                .AlignCenter()
-                .AlignMiddle()
-                .Text("Watermark")
-                .FontSize(48).Bold().FontColor(Colors.Green.Lighten3);
+            // layers
+            //     .Layer()
+            //     .AlignCenter()
+            //     .AlignMiddle()
+            //     .Text("Watermark")
+            //     .FontSize(48).Bold().FontColor(Colors.Green.Lighten3);
 
             layers
                 .Layer()
--- Certificates/obj/Debug/net8.0/Certificates.AssemblyInfo.cs
+++ Certificates/obj/Debug/net8.0/Certificates.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Certificates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0936ede48e1c4dfd2973685ed3b9a659371760b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Certificates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Certificates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
